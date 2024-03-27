87bde1ee3cab4a02056b4e538fd8333d81e68e4a
satelight <crazy_soci80@yahoo.co.jp>
2024-03-27 20:46:49 +0900

    引数にtoにisizeが入るように変更して後ろから文字をスライスできるように変更

--- string_py/src/lib.rs
+++ string_py/src/lib.rs
@@ -8,16 +8,37 @@ impl StrLikePy {
     }
 
     // pythonで言うdata[0..5]に近いやり方を定義
-    pub fn slice(&self,from:usize,to:usize) -> String {
+    pub fn slice(&self,from:isize,to:isize) -> String {
     
         let mut slice_string = String::from("");
     
-        for (i, c )in self.string_data.chars().enumerate(){
-            if from <= i && i < to {
-                slice_string.push(c);
+        if to < 0 {
+            let string_length = (self.string_data.chars().count() as isize + to) as usize;
+            let from = from as usize;
+            for (i, c )in self.string_data.chars().enumerate(){
+                if from <= i && i < string_length {
+                    slice_string.push(c);
+                }
             }
+        } else {
+            let to = to as usize;
+            let from = from as usize;
+            for (i, c )in self.string_data.chars().enumerate(){
+                if from <= i && i < to {
+                    slice_string.push(c);
+                }
+            }
+
         }
 
         slice_string
     }
+}
+
+
+#[test]
+fn string_py_test(){
+    let string_data = StrLikePy::new("あいうえお");
+    let slice_data = string_data.slice(0, -1);
+    println!("{:?}",slice_data);
 }
